f0e1b42808c6477797a4ec0e6007c78e236572af
cibilex <test>
2026-01-08 11:47:46 +0300

    feat: add table

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,62 @@
+import { Button } from '@/components/ui/button'
+import { DataTable } from '@/components/ui/data-table'
 import { createFileRoute } from '@tanstack/react-router'
+import { createColumnHelper } from '@tanstack/react-table'
+import { Trash } from 'lucide-react'
+enum UserTypes {
+  admin = 'admin',
+  manager = 'manager',
+  stuff = 'stuff',
+}
+type User = {
+  id: number
+  name: string
+  type: UserTypes
+}
+const users: User[] = [
+  {
+    id: 1,
+    type: UserTypes.admin,
+    name: 'cibilex',
+  },
+  {
+    id: 2,
+    type: UserTypes.manager,
+    name: 'manager',
+  },
+  {
+    id: 3,
+    type: UserTypes.stuff,
+    name: 'stuff',
+  },
+]
+const columnHelper = createColumnHelper<User>()
+
+const columns = [
+  columnHelper.accessor('name', {
+    id: 'name',
+    cell: (props) => <span>{props.getValue()}</span>,
+    header: () => <span>Name</span>,
+  }),
+  columnHelper.accessor('type', {
+    id: 'type',
+    cell: (props) => <span>{props.getValue()}</span>,
+    header: () => <span>Type</span>,
+  }),
+  columnHelper.display({
+    id: 'actions',
+    cell: () => (
+      <Button variant="destructive" size="icon">
+        <Trash />
+      </Button>
+    ),
+    header: () => <span>Actions</span>,
+  }),
+]
+
+function App() {
+  return <DataTable<User, unknown> columns={columns} data={users}></DataTable>
+}
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -6,7 +64,3 @@ export const Route = createFileRoute('/')({
     console.log('home page loader')
   },
 })
-function App() {
-  console.log('home page')
-  return <div>Home page</div>
-}
